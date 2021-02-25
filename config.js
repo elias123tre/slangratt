49e7d01e0b245d90d6ff82f36bc1fe6ec00f573f
BuildTools <elias123tre>
2021-02-25 01:04:30 +0100

    Tog bort firebase config

---
+++ config.js
@@ -0,0 +1,41 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBm_dmLSFk3klAPQ77DekOtZXhv3eG3ssA",
+  authDomain: "slangratt.firebaseapp.com",
+  projectId: "slangratt",
+  storageBucket: "slangratt.appspot.com",
+  messagingSenderId: "490122733320",
+  appId: "1:490122733320:web:eda8c6446835c8c9316ebd",
+  databaseURL: "https://slangratt-default-rtdb.europe-west1.firebasedatabase.app/",
+}
+
+export default firebaseConfig
+
+// const config = {
+//   apiKey: "AIzaSyBm_dmLSFk3klAPQ77DekOtZXhv3eG3ssA",
+//   authDomain: "slangratt.firebaseapp.com",
+//   projectId: "slangratt",
+//   storageBucket: "slangratt.appspot.com",
+//   messagingSenderId: "490122733320",
+//   appId: "1:490122733320:web:eda8c6446835c8c9316ebd",
+//   databaseURL: "https://slangratt-default-rtdb.europe-west1.firebasedatabase.app/",
+// }
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// <script>
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyBm_dmLSFk3klAPQ77DekOtZXhv3eG3ssA",
+//     authDomain: "slangratt.firebaseapp.com",
+//     projectId: "slangratt",
+//     storageBucket: "slangratt.appspot.com",
+//     messagingSenderId: "490122733320",
+//     appId: "1:490122733320:web:eda8c6446835c8c9316ebd"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+// </script>
