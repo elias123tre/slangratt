960ea9d3545eed8d98d8a6623d1cf1a5b2c80b61
BuildTools <elias123tre>
2021-03-14 02:29:06 +0100

    Tog bort prisma databas

--- pages/api/db.jsx
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
-
-export default async function handle(req, res) {
-  const trash = await prisma.trash.findMany()
-  res.json(trash)
-}
